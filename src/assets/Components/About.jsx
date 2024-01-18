8ad73cdbfa0ec6677c2187a0980367bc46743e11
fariz <farizhazmi5@gmail.com>
2024-01-19 05:40:35 +0700

    Success

--- src/assets/Components/About.jsx
+++ src/assets/Components/About.jsx
@@ -10,14 +10,14 @@ export default function About() {
                     <h4 className="font-poppins mt-[-4px]">Tentang Kami</h4>
                 </div>
             </div>
-            <div className="py-10 px-10">
+            <div className="py-10 px-5">
                 <div className="max-w-[280px] mx-auto">
                     <img src={logoAbout} alt="about-img" />
                 </div>
                 <div className="mt-10">
-                    <p className="font-poppins font-light text-justify max-w-[400px] mx-auto">
+                    <p className="font-poppins font-extralight tracking-wide leading-6">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorem reiciendis perferendis non aut excepturi esse velit laudantium? Accusamus modi ipsum quaerat similique quis perferendis eaque nisi labore
-                        corrupti est! similique quis perferendis eaque nisi labore corrupti est!
+                        corrupti est! corrupti est! corrupti est! corrupti est! corrupti est! similique quis perferendis eaque nisi labore corrupti est!
                     </p>
                 </div>
             </div>
